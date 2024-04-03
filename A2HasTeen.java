e1e4fcdf33274e7281fad86c54793a31e1abd877
ellas <ella.yap26@ycdsbk12.ca>
2024-04-03 10:04:59 -0400

    Updated A2HasTeen.java

--- A2HasTeen.java
+++ A2HasTeen.java
@@ -1,11 +1,27 @@
 class A2HasTeen extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * The program A2HasTeen.java, when given 3 int values via user input, outputs true if 1 or more of them are teen.
+  * A number is "teen" if it is in the range 13 to 19.
+  * @author: E. Yap
   */
   
   public void run() {
-    
+    // Declare variables
+    int intValueOne;
+    int intValueTwo;
+    int intValueThree;
+
+    // Get user input
+    intValueOne = readInt ("Enter age one: ");
+    intValueTwo = readInt ("Enter age two: ");
+    intValueThree = readInt ("Enter age three: ");
+
+    if ((intValueOne >= 13 && intValueOne <= 19) || (intValueTwo >= 13 && intValueTwo <= 19) || (intValueThree >= 13 && intValueThree <= 19)) {
+      System.out.println(true);
+    }
+    else {
+      System.out.println(false);
+    }
   }
 }
--- C2EveryNth.java
+++ C2EveryNth.java
@@ -1,11 +1,32 @@
 class C2EveryNth extends ConsoleProgram {
 
   /**
-  * Description
+  * The program C2EveryNth, when given a non-empty string and an int N,
+  * returns the string made starting with character 0, 
+  * and then every Nth char of the string. 
+  * So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
   * @author: E. Yap
   */
   
   public void run() {
+    // Declare variables
+    String strWord;    
+    String strNewWord;
+    int intNthChar;
+    int intLength;
     
+    // Get word from user
+    strWord = readLine("Enter a word: ");
+    intNthChar = readInt("Enter an integer: ");
+
+    // Initialize varaibles
+    intLength = strWord.length();
+    strNewWord = "";
+
+    for (int i = 0; i <= intLength; i += intNthChar) {
+      strNewWord += strWord.charAt(i);
+    }
+
+    System.out.println(strNewWord);  
   }
 }
